151464e6052e6eca53253ff7799f0bcfd69b1c39
Nick Randell <nick.randell@sceneskope.com>
2018-10-12 18:47:37 +0100

    Initial version

---
+++ Capture/Program.cs
@@ -0,0 +1,37 @@
+﻿using MMALSharp;
+using MMALSharp.Handlers;
+using MMALSharp.Native;
+using System;
+using System.Threading.Tasks;
+
+namespace Capture
+{
+    internal static class Program
+    {
+#pragma warning disable RCS1163 // Unused parameter.
+        private static async Task Main(string[] args)
+#pragma warning restore RCS1163 // Unused parameter.
+        {
+            Console.WriteLine("Hello World!");
+            try
+            {
+                var cam = MMALCamera.Instance;
+                try
+                {
+                    using (var imgCaptureHandler = new ImageStreamCaptureHandler("/home/pi/images/", "jpg"))
+                    {
+                        await cam.TakePicture(imgCaptureHandler, MMALEncoding.JPEG, MMALEncoding.I420);
+                    }
+                }
+                finally
+                {
+                    cam.Cleanup();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+    }
+}
